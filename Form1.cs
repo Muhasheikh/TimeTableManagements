e69e2567ea3e685e3cf1b6ea89558b6ab8d6d73f
Muhasheikh <muhasheikh8@gmail.com>
2021-06-03 13:48:41 +0530

    added comments

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,7 @@ namespace StudentsAndTagsManagement
 {
     public partial class Form1 : Form
     {
+        //declaring database connection
         SqlConnection con = new SqlConnection(@"Server=tcp:abcinstitute.database.windows.net,1433;Initial Catalog=time-table;Persist Security Info=False;User ID=biyoni;Password=itpm12345@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         SqlCommand cmd;
         SqlDataAdapter adapt;
